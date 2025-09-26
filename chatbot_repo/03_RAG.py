5e5a19b738ace3168892b823a0c77f1f7cd28207
woojin103 <47294748+woojin103@users.noreply.github.com>
2025-09-26 18:03:21 +0900

    Update 03_RAG.py

--- chatbot_repo/03_RAG.py
+++ chatbot_repo/03_RAG.py
@@ -85,7 +85,7 @@ embeddings = HuggingFaceEmbeddings(
 
 # ChromaDB 로드 및 Retriever 설정
 
-chroma_persist_dir = r"C:\Users\PC1\OneDrive\프로젝트\250801_아모레\chroma_db"
+chroma_persist_dir = r"C:\Users\PC1\amore_chatbot\chatbot_repo\data"
 db = Chroma(persist_directory=chroma_persist_dir, embedding_function=embeddings)
 retriever = db.as_retriever(search_kwargs={"k": 3})
 print("DB를 Retriever로 설정했습니다.\n")
